6a55e99a1269e18310eeec26307ebd4637bd855c
VladimirBerl <vova2002.13lol@gmail.com>
2025-07-31 20:25:45 +0300

    fix cors

--- server/middleware/cors.ts
+++ server/middleware/cors.ts
@@ -1,5 +1,10 @@
 export default defineEventHandler((event) => {
-  setHeader(event, 'Access-Control-Allow-Origin', 'https://delightedly-original-shrike.cloudpub.ru');
+  setHeader(event, 'Access-Control-Allow-Origin', '*');
   setHeader(event, 'Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   setHeader(event, 'Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  if (event.method === 'OPTIONS') {
+    setResponseStatus(event, 200);
+    return '';
+  }
 });
